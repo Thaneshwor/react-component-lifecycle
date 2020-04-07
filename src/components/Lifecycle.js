e0bf817f1aa621dc29748c4c2fdf69fda189bf32
Thaneshwor <thaneshwor100@gmail.com>
2020-04-07 23:59:57 +0545

    first commit

---
+++ src/components/Lifecycle.js
@@ -0,0 +1,14 @@
+class Lifecycle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { favoritecolor: "red" };
+    }
+    static getDerivedStateFromProps(props, state) {
+        return { favoritecolor: props.favcol };
+    }
+    render() {
+        return (
+            <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+        );
+    }
+}
